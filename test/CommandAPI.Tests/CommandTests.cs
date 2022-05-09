34d75a824338f81c3b747b8e09c617248c3c0d9c
Kevin Grems <kevin.grems@live.com>
2022-05-09 09:49:48 -0500

    e

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandAPI.Models;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        private Command TestCommand;
+
+        public CommandTests()
+        {
+            TestCommand = new Command()
+            {
+                HowTo = "Do something",
+                Platform = "Some platform",
+                CommandLine = "Some commandline"
+            };
+        }
+
+        public void Dispose()
+        {
+            TestCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            
+
+            TestCommand.HowTo = "Execute unit tests";
+            Assert.Equal("Execute unit tests", TestCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+
+            TestCommand.Platform = "xUnit not gUnit";
+            Assert.Equal("xUnit not gUnit", TestCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+
+            TestCommand.CommandLine = "dotnet test";
+            Assert.Equal("dotnet test", TestCommand.CommandLine);
+        }
+    }
+}
---
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CommandAPI.Controllers;
+using CommandAPI.Data;
+using CommandAPI.Dtos;
+using CommandAPI.Models;
+using CommandAPI.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandsControllerTests : IDisposable
+    {
+        Mock<ICommandAPIRepo> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+        }
+
+        [Fact]
+        public void GetCommandItems_Returs200OK_WhenDBisEmpty()
+        {
+            // Arrange
+            var mockRepo = new Mock<ICommandAPIRepo>();
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(0));
+
+            var realProfile = new CommandsProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+
+            IMapper mapper = new Mapper(configuration);
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAllCommands();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetAllCommands_ReturnsOneItem_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAllCommands();
+
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+            var commands = okResult.Value as List<CommandReadDto>;
+            Assert.Single(commands);
+        }
+
+        [Fact]
+        public void GetAllCommands_Returns200OK_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+                repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetAllCommands_ReturnsCorrectType_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Assert
+            Assert.IsType<ActionResult<IEnumerable<CommandReadDto>>>(result);
+        }
+
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command
+                {
+                    Id = 0,
+                    HowTo = "How to generate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".Net Core EF"
+                });
+            }
+
+            return commands;
+        }
+    }
+}
